fde2a1fc3ee13a2c86aebf09cba4877bc00e4c00
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-12-17 16:05:38 -0300

    First commit

---
+++ src/main/java/com/guilhermezuriel/reduceme/ReducemeApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermezuriel.reduceme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReducemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReducemeApplication.class, args);
+    }
+
+}
