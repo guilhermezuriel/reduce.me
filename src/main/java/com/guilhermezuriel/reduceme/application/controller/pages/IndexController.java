14370a03bf972f9756fedf5332b59783f55a150b
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-01-17 16:36:02 -0300

    feature - page html

---
+++ src/main/java/com/guilhermezuriel/reduceme/application/controller/pages/IndexController.java
@@ -0,0 +1,18 @@
+package com.guilhermezuriel.reduceme.application.controller.pages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping("/home")
+    public String indexHtml(Model model) {
+        model.addAttribute("url");
+        return "index";
+    }
+}
