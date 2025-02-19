fe8f0a72bec687ea2a760aafc9fa5858f25d3730
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-02-18 22:36:31 -0300

    removing imports unused

--- src/main/java/com/guilhermezuriel/reduceme/application/config/migrations/RunCassandraMigrations.java
+++ src/main/java/com/guilhermezuriel/reduceme/application/config/migrations/RunCassandraMigrations.java
@@ -5,7 +5,6 @@ import com.datastax.oss.driver.api.core.cql.BoundStatement;
 import com.datastax.oss.driver.api.core.cql.ColumnDefinitions;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
-import com.guilhermezuriel.reduceme.application.config.exceptions.ApplicationException;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,6 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
